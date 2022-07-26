2858c85a99487b15fc5bd871673164d07a8ecdc6
phani <phanendrasai41@gmail.com>
2022-07-26 14:21:15 +0530

    updating ./practice-roboshop-ansible

--- Launch-instance-script/create-ec2.sh
+++ Launch-instance-script/create-ec2.sh
@@ -21,7 +21,7 @@ create_ec2() {
 }
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-AMI_ID="ami-0671e931bd9952785"
+AMI_ID="ami-06585c3e85ffe675f"
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 if [ "$COMPONENT" == "all" ]; then
