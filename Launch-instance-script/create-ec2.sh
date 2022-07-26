394a8d258879d8a9bbe6b43640b044e988f8255e
phani <phanendrasai41@gmail.com>
2022-07-26 12:55:52 +0530

    updating ./practice-roboshop-ansible

---
+++ Launch-instance-script/create-ec2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+##### Change these values ###
+ZONE_ID="Z078807431WCP5BA26ASG"
+SG_NAME="allow-all-to-public"
+#############################
+
+
+COMPONENT=all
+create_ec2() {
+  PRIVATE_IP=$(aws ec2 run-instances \
+      --image-id ${AMI_ID} \
+      --instance-type t2.micro \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+      --security-group-ids ${SGID} \
+      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+}
+
+#AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+AMI_ID="ami-0671e931bd9952785"
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+
+if [ "$COMPONENT" == "all" ]; then
+  for component in catalogue-1 cart-1 dispatch-1 user-1 shipping-1 payment-1 frontend-1 mongodb-1 mysql-1 rabbitmq-1 redis-1 ; do
+    COMPONENT=$component
+    create_ec2
+  done
+else
+  create_ec2
+fi
