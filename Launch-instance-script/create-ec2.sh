b73717f5e669bf9328681a8afcf82338b97e6b62
phani <phanendrasai41@gmail.com>
2022-08-02 19:06:28 +0530

    updating ./practice-roboshop-ansible

--- Launch-instance-script/create-ec2.sh
+++ Launch-instance-script/create-ec2.sh
@@ -3,6 +3,7 @@
 ##### Change these values ###
 ZONE_ID="Z078807431WCP5BA26ASG"
 SG_NAME="allow-all-to-public"
+IAM_INSTANCE_PROFILE="Arn=arn:aws:iam::645019601948:instance-profile/role-for-aws-secret-manager-for-roboshop-components"
 #############################
 
 
@@ -14,6 +15,7 @@ create_ec2() {
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
+      --iam-instance-profile="${IAM_INSTANCE_PROFILE}" \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
   sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
